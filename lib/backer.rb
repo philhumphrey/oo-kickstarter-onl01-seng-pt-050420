03d70172493682a5aa8bb31762344de71b517e3c
Phil Humphrey <phumphrey588@yahoo.com>
2020-06-04 10:20:56 +0000

    Automatically backed up by Learn

---
+++ lib/backer.rb
@@ -0,0 +1,19 @@
+
+class Backer
+  attr_accessor 
+  attr_reader :backed_projects, :name
+  
+  def initialize(name)
+    @name = name 
+  @backed_projects = []
+end
+
+def back_project(project)
+  @backed_projects << project
+  # .all.select  {|x| x.project == self}
+
+  # def more_advanced_back_project
+  project.add_backer(self) unless project.backers.include?(self)
+  end
+end
+  
---
+++ lib/project.rb
@@ -0,0 +1,15 @@
+
+class Project
+  attr_accessor 
+  attr_reader :backers, :title
+  
+  def initialize(title)
+    @title = title
+    @backers = []
+ end  
+ def add_backer(backer)
+   @backers << backer
+  # backer.add_projects(self) unless backer.backed_projects.include?(self)
+  end
+end
+  
