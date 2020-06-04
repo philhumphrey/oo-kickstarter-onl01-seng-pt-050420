1a5d0509d797889cdd6daaab7b4832dbd6d6a6cc
Phil Humphrey <phumphrey588@yahoo.com>
2020-06-04 10:29:05 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -11,8 +11,7 @@ class Project
  def add_backer(backer)
      def add_backer(backer)
     @backers << backer
-    backer.back_project(self) unless backer.backed_projects.include?(self)
-    end
+    # backer.back_project(self) unless backer.backed_projects.include?(self)
   end
   end
   
